55d1ade137238d0341e316ed722664195e8296cc
Chris Meyer <cmeyer1969@gmail.com>
2016-10-20 15:34:17 -0700

    Remove redundant tests.

--- nion/data/test/Core_test.py
+++ nion/data/test/Core_test.py
@@ -7,7 +7,6 @@ import numpy
 
 # local libraries
 from nion.data import Calibration
-from nion.data import Context
 from nion.data import Core
 from nion.data import DataAndMetadata
 
@@ -19,31 +18,6 @@ class TestCore(unittest.TestCase):
 
     def tearDown(self):
         pass
-
-    def test_something(self):
-        ctx = Context.context()
-        src_data = ((numpy.abs(numpy.random.randn(12, 8)) + 1) * 10).astype(numpy.uint32)
-        a = DataAndMetadata.DataAndMetadata.from_data(src_data)
-        b = ctx.line_profile(a, ctx.vector(ctx.normalized_point(0.25, 0.25), ctx.normalized_point(0.5, 0.5)), 2)
-        # print(b.data)
-
-    def test_something_else(self):
-        ctx = Context.context()
-        src_data = ((numpy.abs(numpy.random.randn(12, 8)) + 1) * 10).astype(numpy.int32)
-        a = DataAndMetadata.DataAndMetadata.from_data(src_data)
-        b = ctx.amin(a) - a
-        # print(a.data)
-        # print(b.data_shape)
-        # print(b.data)
-
-    def test_something_again(self):
-        ctx = Context.context()
-        src_data = ((numpy.abs(numpy.random.randn(12, 8)) + 1) * 10).astype(numpy.uint32)
-        a = DataAndMetadata.DataAndMetadata.from_data(src_data)
-        b = a - ctx.amin(a)
-        # print(a.data)
-        # print(b.data_shape)
-        # print(b.data)
 
     def test_fft_produces_correct_calibration(self):
         src_data = ((numpy.abs(numpy.random.randn(16, 16)) + 1) * 10).astype(numpy.float)
