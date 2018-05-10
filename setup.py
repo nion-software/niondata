9295b15889f8477967612e92f433a1db3ea978af
Chris Meyer <cmeyer1969@gmail.com>
2018-05-10 13:03:46 -0700

    Add section to deploy to PyPI on tags. Add conda config meta.yaml.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setuptools.setup(
     name="niondata",
-    version="0.1.0",
+    version="0.13.0",
     packages=["nion.data", "nion.data.test"],
     install_requires=['scipy', 'numpy', 'nionutils'],
     classifiers=[
